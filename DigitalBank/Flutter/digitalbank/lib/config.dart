4484eaf199211e4e3306573cadb77b3e09b18f70
Hamza AIT BEN YISSA <hamzaaitbenyissa@gmail.com>
2022-06-05 02:40:16 +0100

    fix some settings

--- DigitalBank/Flutter/digitalbank/lib/config.dart
+++
@@ -1,4 +0,0 @@
-class Config {
-  final String host = "https://5d72-105-72-7-62.eu.ngrok.io";
-  final String domain = "5d72-105-72-7-62.eu.ngrok.io";
-}
--- DigitalBank/Flutter/digitalbank/lib/controllers/auth_controller.dart
+++ DigitalBank/Flutter/digitalbank/lib/controllers/auth_controller.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:digitalbank/config.dart';
+import 'package:digitalbank/configuration/config.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
